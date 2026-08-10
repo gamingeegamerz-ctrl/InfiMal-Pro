c9d08529a68b8ef5150330804061986fb077c23b
gamingeegamerz-ctrl <gamingeegamerz@gmail.com>
2026-08-10 19:35:05 +0530

    Fix duplicate password reset notification

--- app/Notifications/Auth/ResetPasswordNotification.php
+++ app/Notifications/Auth/ResetPasswordNotification.php
@@ -10,9 +10,6 @@ class ResetPasswordNotification extends ResetPassword
 {
     use Queueable;
 
-    /**
-     * Build the mail representation of the notification.
-     */
     public function toMail($notifiable): MailMessage
     {
         $resetUrl = url(route('password.reset', [
@@ -21,24 +18,19 @@ class ResetPasswordNotification extends ResetPassword
         ], false));
 
         $appName = config('app.name', 'InfiMal');
-        $expiryMinutes = (int) config('auth.passwords.'.config('auth.defaults.passwords').'.expire', 60);
+        $expiryMinutes = (int) config(
+            'auth.passwords.' . config('auth.defaults.passwords') . '.expire',
+            60
+        );
 
         return (new MailMessage)
             ->subject("{$appName} Password Reset Request")
-            ->greeting('Hi '.($notifiable->name ?: 'there').',')
+            ->greeting('Hi ' . ($notifiable->name ?: 'there') . ',')
             ->line('We received a request to reset your password.')
             ->line('Click the button below to set a new password securely.')
             ->action('Reset Password', $resetUrl)
             ->line("This reset link will expire in {$expiryMinutes} minutes.")
             ->line('If you did not request this, you can safely ignore this email.')
             ->salutation("Regards,\n{$appName} Security Team");
-        return (new MailMessage)
-            ->subject('Reset your InfiMal password')
-            ->greeting('Hello!')
-            ->line('We received a request to reset your InfiMal account password.')
-            ->action('Reset Password', $resetUrl)
-            ->line('This password reset link will expire in 60 minutes.')
-            ->line('If you did not request a password reset, no further action is required.')
-            ->salutation('Regards, InfiMal Security Team');
     }
 }
